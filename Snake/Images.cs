3fefacf3ff34ee801b362441c5b96c870bfc6c30
cozyGarage <e1500953@edu.vamk.fi>
2023-05-27 00:13:47 +0300

    add comment

--- Snake/Images.cs
+++ Snake/Images.cs
@@ -10,14 +10,16 @@ namespace Snake
 {
     public static class Images
     {
-        public readonly static ImageSource Empty = LoadImage("Empty.png");
-        public readonly static ImageSource Body = LoadImage("Body.png");
-        public readonly static ImageSource Head = LoadImage("Head.png");
-        public readonly static ImageSource Food = LoadImage("Food.png");
-        public readonly static ImageSource DeadBody = LoadImage("DeadBody.png");
-        public readonly static ImageSource DeadHead = LoadImage("DeadHead.png");
+                                                                                                        // Static fields to hold the image sources
+        public readonly static ImageSource Empty = LoadImage("Empty.png");                              // Image for empty grid cells
+        public readonly static ImageSource Body = LoadImage("Body.png");                                // Image for snake body
+        public readonly static ImageSource Head = LoadImage("Head.png");                                // Image for snake head
+        public readonly static ImageSource Food = LoadImage("Food.png");                                // Image for food
+        public readonly static ImageSource DeadBody = LoadImage("DeadBody.png");                        // Image for dead snake body
+        public readonly static ImageSource DeadHead = LoadImage("DeadHead.png");                        // Image for dead snake head
 
-        private static ImageSource LoadImage(string fileName)
+                                                                                                
+        private static ImageSource LoadImage(string fileName)                                       // Method to load an image from the assets folder
         {
             return new BitmapImage(new Uri($"assets/{fileName}", UriKind.Relative));
         }

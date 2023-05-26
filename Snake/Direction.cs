6d604a4d3dfeca1532ef908e93070d7c1cee4867
cozyGarage <e1500953@edu.vamk.fi>
2023-05-26 23:25:54 +0300

    add comment

--- Snake/Direction.cs
+++ Snake/Direction.cs
@@ -8,10 +8,10 @@ namespace Snake
 {
     public class Direction                                                                              // Class to control how the snake moves
     {
-        public readonly static Direction Left = new(0, -1);
-        public readonly static Direction Right = new(0, 1);
-        public readonly static Direction Up = new(-1, 0);
-        public readonly static Direction Down = new(1, 0);
+        public readonly static Direction Left = new(0, -1);                                             // Directions row value is unchanged, column value is offset by -1
+        public readonly static Direction Right = new(0, 1);                                             // Directions row value is unchanged, column value is offset by 1
+        public readonly static Direction Up = new(-1, 0);                                               // Directions row value is offset by -1, column value is unchanged
+        public readonly static Direction Down = new(1, 0);                                              // Directions row value is offset by 1, column value is unchanged
 
         public int RowOffset { get; }
         public int ColumnOffset { get; }
@@ -34,7 +34,7 @@ namespace Snake
                    ColumnOffset == direction.ColumnOffset;
         }
 
-        public override int GetHashCode()
+        public override int GetHashCode()                                                               // Method to define the hash code of the direction gererated by the Visual Studio IDE
         {
             return HashCode.Combine(RowOffset, ColumnOffset);
         }

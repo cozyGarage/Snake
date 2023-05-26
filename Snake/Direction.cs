0a3bbdd9536db3bdb3188dcdc6c049d47a157746
cozyGarage <e1500953@edu.vamk.fi>
2023-05-26 23:13:37 +0300

    add comment

--- Snake/Direction.cs
+++ Snake/Direction.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    public class Direction
+    public class Direction                                                                              // Class to control how the snake moves
     {
         public readonly static Direction Left = new(0, -1);
         public readonly static Direction Right = new(0, 1);
@@ -16,18 +16,18 @@ namespace Snake
         public int RowOffset { get; }
         public int ColumnOffset { get; }
 
-        private Direction(int rowOffset, int columnOffset)
+        private Direction(int rowOffset, int columnOffset)                                              // Constructor to define the direction
         {
             RowOffset = rowOffset;
             ColumnOffset = columnOffset;
         }
 
-        public Direction Opposite()
+        public Direction Opposite()                                                                     // Method to define the opposite direction
         {
             return new Direction(-RowOffset, -ColumnOffset);
         }
 
-        public override bool Equals(object obj)
+        public override bool Equals(object obj)                                                             // Method to define the equality of the direction
         {
             return obj is Direction direction &&
                    RowOffset == direction.RowOffset &&
